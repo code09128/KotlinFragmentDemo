09356c2106641f022b4eda122b5b9ebe06a7107b
dustin <dustin0128@24drs.com>
2020-07-03 14:21:31 +0800

    first commit

---
+++ app/src/main/java/example/givemepass/fragmentdemo/MainActivity.kt
@@ -0,0 +1,41 @@
+package example.givemepass.fragmentdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val firstFragment = DetailsFragment()
+    private val secondFragment = TEstFragment()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        addFragment(firstFragment)
+//        addFragment(secondFragment)
+
+        first_fragment.setOnClickListener {
+//            replaceFragment(firstFragment)
+            addFragment(firstFragment)
+        }
+
+        second_fragment.setOnClickListener {
+            replaceFragment(secondFragment)
+        }
+    }
+
+    private fun addFragment(f: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fragment_container, f)
+        transaction.commit()
+    }
+
+    private fun replaceFragment(f : Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, f)
+        transaction.commit()
+    }
+}
